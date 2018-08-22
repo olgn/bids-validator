6103f13d6e36f7d4be4ffeb5e7b7ccd459a2e552
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-08-22 12:01:55 -0700

    move from gulp-uglify to terser, which supports es6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@
         source = require('vinyl-source-stream'),
         buffer = require('vinyl-buffer'),
         babelify = require('babelify'),
-        uglify = require('gulp-uglify'),
+        terser = require('gulp-terser'),
         del = require('del'),
         notify = require('gulp-notify'),
         browserSync = require('browser-sync'),
@@ -96,7 +96,7 @@
             .pipe(source(p.bundle))
             .pipe(buffer())
             .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(uglify())
+            .pipe(terser())
             .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest(p.dist));
     });
